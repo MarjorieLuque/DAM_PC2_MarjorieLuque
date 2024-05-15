ef17741f5b238d619aab91b4a037534101c21774
Marjorie Luque <luquecardenasmarjorienikhol@gmail.com>
2024-05-15 10:32:40 -0500

    Tercer commit

--- DAM_MarjorieLuque/app/src/main/java/com/example/dam_marjorieluque/MainActivity.java
+++ DAM_MarjorieLuque/app/src/main/java/com/example/dam_marjorieluque/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.dam_marjorieluque;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); // Aquí debe ir setContentView()
+
+        // Ahora puedes acceder a las vistas después de haber establecido el layout
+        Button btn1 = findViewById(R.id.btnEnviar);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Se envió sus datos", Toast.LENGTH_SHORT).show());
     }
-
-
-
 }
